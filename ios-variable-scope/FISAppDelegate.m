a5729729b50b388bb8b1160e8436e38d6c98ffff
markedwardmurray <markedwardmurray@gmail.com>
2015-06-23 17:43:19 -0400

    removed solution

--- ios-variable-scope/FISAppDelegate.m
+++ ios-variable-scope/FISAppDelegate.m
@@ -19,52 +19,10 @@
     return YES;
 }
 
-- (NSMutableArray *)arrayByAddingString:(NSString *)string toArray:(NSMutableArray *)array {
-    
-    NSMutableArray *newArray = [array mutableCopy];
-    
-    [newArray addObject:string];
-
-    return newArray;
-}
-
-// Solution that uses NSCharacterSet
-
-//- (NSUInteger)countOfStringsInAllCapsInArray:(NSArray *)array {
-//    
-//    NSMutableArray *stringsInAllCaps = [@[] mutableCopy];
-//    
-//    NSCharacterSet *lowercaseSet = [NSCharacterSet lowercaseLetterCharacterSet];
-//    
-//    for (NSUInteger i = 0; i < array.count; i++) {
-//        NSString *currentString = array[i];
-//        NSString *capitalsOnly = [currentString stringByTrimmingCharactersInSet:lowercaseSet];
-//        if ([currentString isEqualToString:capitalsOnly]) {
-//            [stringsInAllCaps addObject:currentString];
-//        }
-//    }
-//    
-//    return stringsInAllCaps.count;
-//}
-
-- (NSUInteger)countOfUppercaseStringsInArray:(NSArray *)array {
-    NSMutableArray *uppercaseStrings = [@[] mutableCopy];
-    
-    for (NSUInteger i = 0; i < array.count; i++) {
-        NSString *currentString = array[i];
-        NSString *upperString = [currentString uppercaseString];
-
-        if ([currentString isEqualToString:upperString]) {
-            [uppercaseStrings addObject:currentString];
-        }
-    }
-    
-    return uppercaseStrings.count;
-}
-
-- (void)removeAllObjectsFromArray:(NSMutableArray *)array {
-    [array removeAllObjects];
-}
-
+/*
+ 
+ * Write your method definitions here.
+ 
+ */
 
 @end
